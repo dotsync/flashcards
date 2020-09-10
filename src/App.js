98b7530af283da59901d09b67850e5760b32273d
Sergei Liddell <liddell.s@pm.me>
2020-09-10 17:24:49 -0600

    Removes boilerplate code and logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 export default function App() {
   return (
     <div>
-      <h1>hello</h1>
+      <h1>Flashcards</h1>
     </div>
   )
 }
--- src/App.test.js
+++ src/App.test.js
@@ -12,11 +12,11 @@ describe('App - Basic Tests', () => {
   // The next two lines keep track of the total amount of tests performed
   let totalBasicTestsPerformed = 0;
   totalBasicTestsPerformed ++;
-  test(`renders the title of App [component:App] [Test-1-of-${totalBasicTestsPerformed}]`,
+  test(`It renders the title of App [component:App] [Basic-test-1-of-${totalBasicTestsPerformed}]`,
    () => {
     // Increase totalBasicTestsPerformed by one for the test that follows
     totalBasicTestsPerformed ++;
     const wrapper = shallow(<App />);
-    expect(wrapper.find('h1').text()).toContain('hello');
+    expect(wrapper.find('h1').text()).toContain('Flashcards');
   });
 });
