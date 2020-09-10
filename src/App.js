66f15b1b4a9f537c63900a5e7b566cb3d77faf59
Sergei Liddell <liddell.s@pm.me>
2020-09-10 17:13:21 -0600

    Refactors App component
    * No significant Changes* Made to be functional
    because want to do the entire App the same way.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
+    <div>
       <h1>hello</h1>
     </div>
-  );
+  )
 }
-
-export default App;
