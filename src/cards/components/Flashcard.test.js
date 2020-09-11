da179d243452b3248051f0aa5a2a2fc9755db850
Sergei Liddell <liddell.s@pm.me>
2020-09-11 13:54:34 -0600

    Creates two tests for Flashcards components
    * Checks if, myFlashcard has a question.
    * Checks if, myFlashcard has a subject.
    * Checks if, myFlashcard has a category.

--- src/cards/components/Flashcard.test.js
+++ src/cards/components/Flashcard.test.js
@@ -12,6 +12,8 @@ import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
+
+
 describe('Flashcard Component', () => {
   let totalBasicTestsPerformed = 0;
   let container = null;
@@ -27,19 +29,51 @@ describe('Flashcard Component', () => {
     // get ready to recieve next mount
     container = null;
   });
-  test('It renders without a category', () => {
-    act(() => {render(<Flashcard />, container)})
-    expect(container.textContent).toBe('no flashcard passed')
-    console.log(`
-    ----------------start----------------
-    CONSOLE.LOGS from FLASHCARD component
-     -> log 01: ${'insert var to log here'}
-     -> log 02: ${util.inspect(Flashcard, false, null, true /* enable colors */)}`)
+  // minProps
+  const testCard = {
+    id: 1,
+    createdAt: "2018-8-3 11:12:40",
+    category: 'math',
+    subject: 'addition',
+    question: 'what is 1 plus 2 ?',
+    answer: '3',
+    multipleChoice: ['2', '3', '4', '5']
+  }
+
+  describe('It renders without a flashcard prop', () => {
+    test('It renders loading message', () => {
+      act(() => { render(<Flashcard />, container) });
+      expect(container.textContent).toBe('no flashcard passed');
+    });
+  });
+  // passes a dummy to myFlashcards prop
+  describe('It renders with a mock flashcard', () => {
+    test('myFlashcard has a category', () => {
+      const component = shallow(<Flashcard myFlashcards={testCard}/>)
+      const wrapper = component.find('.makeStyles-category-8')
+      // exact match
+      expect(wrapper.text()).toBe('math')
+    });
+    test('myFlashcard has a subject', () => {
+      const component = shallow(<Flashcard myFlashcards={testCard}/>)
+      const wrapper = component.find('.makeStyles-subject-9')
+      expect(wrapper.text()).toBe('addition')
+    });
+    test('myFlashcard has a question', () => {
+      const component = shallow(<Flashcard myFlashcards={testCard}/>)
+      const wrapper = component.find('.makeStyles-question-10')
+      expect(wrapper.text()).toBe('what is 1 plus 2 ?')
+    });
   });
 
-  // unfulfilled tests
-  // test('It has a subject', () => { })
-  // test('It has a question', () => { })
+
+
+
+
+  describe('It renders a flashcard from an API', () => {
+    test('It GETS flashcards from an API', () => {})
+  });
+  // unfulfilled tests TODO
   // test('It has a button to flip card', () => { })
   // test('It has a button to like card', () => { })
   // test('It has a button to display hints', () => { })
