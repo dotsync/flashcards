d29466f7bce8edd7bbec20fc6336694e0ae7bed9
Sergei Liddell <liddell.s@pm.me>
2020-09-10 18:56:56 -0600

    Creates Cards routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 // routes
 import Users from "./users/pages/Users";
+import Cards from "./cards/pages/Cards";
 
 export default function App() {
   return (
     <div>
     <h1>Flashcards</h1>
     <Router>
+      <Switch>
+      <Route path="/cards" >
+        <Cards />
+      </Route>
       <Route path="/users" >
         <Users />
       </Route>
+      <Redirect to="/"/>
+      </Switch>
     </Router>
     </div>
   )
--- src/cards/pages/Cards.js
+++ src/cards/pages/Cards.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import MyCards from "../components/MyCards";
+
 export default function Cards() {
   return (
     <div>
       <h1>Cards</h1>
+      <MyCards />
     </div>
   )
 }
