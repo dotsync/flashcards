80bb835b72cbc4eb114af1e6d4b21266b4cc2ee9
Sergei Liddell <liddell.s@pm.me>
2020-09-10 18:15:50 -0600

    Adds route to Users page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+// routes
 import Users from "./users/pages/Users";
 
 export default function App() {
   return (
+    <div>
+    <h1>Flashcards</h1>
     <Router>
-      <Route path="/" >
+      <Route path="/users" >
         <Users />
       </Route>
     </Router>
+    </div>
   )
 }
