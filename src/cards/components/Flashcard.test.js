f4f391e1fb861273c9439c02be8325e6222dd5ee
Sergei Liddell <liddell.s@pm.me>
2020-09-11 09:24:26 -0600

    Adds 3 more tests for Flashcard component

--- src/cards/components/Flashcard.test.js
+++ src/cards/components/Flashcard.test.js
@@ -12,6 +12,10 @@ describe('Flashcard Component', () => {
   let totalBasicTestsPerformed = 0;
 
   // unfulfilled tests
+  test('It has a category', () => {})
+  test('It has a subject', () => {})
   test('It has a question', () => {})
   test('It has a button to flip card', () => {})
+  test('It has a button to like card', () => {})
+  test('It has a button to display hints', () => {})
 });
