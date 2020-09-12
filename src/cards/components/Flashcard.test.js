ec344cce5277a7ab8b3e101edc50cd2f6e4bb27b
Sergei Liddell <liddell.s@pm.me>
2020-09-11 20:53:26 -0600

    Refactors tests to use a setUp function

--- src/cards/components/Flashcard.test.js
+++ src/cards/components/Flashcard.test.js
@@ -12,24 +12,8 @@ import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
-
-
 describe('Flashcard Component', () => {
-  let totalBasicTestsPerformed = 0;
-  let container = null;
-  // setup a DOM element as a render target
-  beforeEach(() => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-  });
-  // cleanup on exiting
-  afterEach(() => {
-    unmountComponentAtNode(container);
-    container.remove();
-    // get ready to recieve next mount
-    container = null;
-  });
-  // minProps
+  // dummy
   const testCard = {
     id: 1,
     createdAt: "2018-8-3 11:12:40",
@@ -40,43 +24,59 @@ describe('Flashcard Component', () => {
     multipleChoice: ['2', '3', '4', '5']
   }
 
+  // start tests
   describe('It renders without a flashcard prop', () => {
     test('It renders loading message', () => {
-      act(() => { render(<Flashcard />, container) });
-      expect(container.textContent).toBe('no flashcard passed');
+      const component = shallow(<Flashcard />)
+      const wrapper = component.find('div')
+      console.log(component.debug())
+      expect(wrapper.text()).toBe('no flashcard passed');
     });
   });
-  // passes a dummy to myFlashcards prop
+  // setup
+  const setUp = (props = {}) => {
+    // passes a dummy to myFlashcards prop
+    const component = shallow(<Flashcard myFlashcards={testCard} />)
+    return component
+  }
+  // pre drill
+  let component
+  beforeEach(() => {
+    component = setUp()
+  })
+  // start tests
   describe('It renders a complete Flashcard using dummy myFlashcard', () => {
     test('myFlashcard renders a category', () => {
-      const component = shallow(<Flashcard myFlashcards={testCard} />)
-      const wrapper = component.find('.makeStyles-category-7')
+      const wrapper = component.find('.makeStyles-category-2')
       // exact match
       expect(wrapper.text()).toBe('Catagory: math')
     });
     test('myFlashcard renders a subject', () => {
-      const component = shallow(<Flashcard myFlashcards={testCard} />)
-      const wrapper = component.find('.makeStyles-subject-8')
+      const wrapper = component.find('.makeStyles-subject-3')
       expect(wrapper.text()).toBe('Subject: addition')
     });
+
+
     test('myFlashcard renders a question', () => {
-      const component = shallow(<Flashcard myFlashcards={testCard} />)
-      const wrapper = component.find('.makeStyles-question-10')
+      const wrapper = component.find('.makeStyles-question-5')
       expect(wrapper.text()).toBe('what is 1 plus 2 ?')
     });
   });
   // Checks myFlashcard has buttons
   describe('It has buttons', () => {
     test('Flashcard renders reveal answer button', () => {
-      const component = shallow(<Flashcard myFlashcards={testCard}/>)
-      const wrapper = component.find('.makeStyles-answerButton-9')
+      const wrapper = component.find('.makeStyles-answerButton-4')
       expect(wrapper.length).toBe(1)
     });
-    // test('It has a button to flip card', () => { })
+    test('Flashcard registers click', () => {
+      const wrapper = component.find('.makeStyles-answerButton-4')
+      expect(wrapper.text()).toBe("0")
+    })
     // test('It has a button to like card', () => { })
     // test('It has a button to display hints', () => { })
   });
   // API
+  // setup(mount?)
   // describe('It renders a flashcard using an API', () => {
   //   // let cardFromAPI
   //   test('myFlashcard renders a category', () => {
